4de7876d10e7d0786f8b360edb5d170e58d05f36
Nataly Kuzminska <kuzminskanataly@gmail.com>
2021-08-06 17:18:11 +0300

    change js

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 'use strict';
 let question = confirm('Угадай число от 1 до 100');
 let enter = prompt('Введи число от 1 до 100');
-let answer = 0;
-let randomNumber = Math.floor(Math.random()*100) + 1;
+let answer;
+let number;
+let getStatusAnswer;
 
 let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+let randomNumber = function() {
+number = Math.floor(Math.random()*100) + 1;
+
 let getStatusAnswer = function() {
-  
-  if(answer > randomNumber) {
+  if(answer > number) {
     alert('Загаданное число меньше');
-  } else if(answer < randomNumber) {
+  } else if(answer < number) {
     alert('Загаданное число больше');
   } else if(!isNumber(answer)) {
     alert('Введи число'); 
-  } else if(answer === randomNumber) {
+  } else if(answer === number) {
     alert('Поздравляю, вы выиграли!');
   } else {
     alert('Игра окончена!');
   }
   getStatusAnswer(answer);
 }; 
+};
 
 isNumber();
+randomNumber(number);
+getStatusAnswer(answer);
 
 console.log(answer);
